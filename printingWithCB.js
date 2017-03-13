4952b73d01ad093122e3976de114cc43fdaacde1
davidliang781205 <davidliang781205@gmail.com>
2017-03-13 20:25:35 +0000

    Added printingWithCB

---
+++ printingWithCB.js
@@ -0,0 +1,35 @@
+var https = require('https');
+var result = '';
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+
+    response.on('error', (error) => console.log(error));
+
+
+    response.on('data', (data) => (result += data ));
+
+
+    response.on('end', (end) => callback(result));
+
+
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+getHTML(requestOptions, printHTML);
